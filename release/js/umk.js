caa59701840bf93ff108391272221c3b54d97c4b
Тютюник Сергей <tutser.job@yandex.ru>
2020-05-22 19:20:17 +0300

    исправил появление меню в тесте

--- release/js/umk.js
+++ release/js/umk.js
@@ -8,6 +8,34 @@ $(document).ready(function () {
 
   // }
 
+  function HideTest(display_style) {
+
+    // current_style = document.getElementById("current-style").style.display;
+    // element = document.getElementById('current-style');
+    // display_style = window.getComputedStyle(element).display;
+
+    element = $('#current-style');
+    d_style = element.css('display');
+
+    if (d_style == "block") element.css("display", "none");
+    else element.css("display", "block");
+    // return display_style;
+    console.log(d_style);
+
+    // if (Element.
+    //   style.color == 'green') Element.style.color = 'red';
+    // else Element.style.color = 'green';
+    // return false;
+  }
+
+
+
+
+  // $('.menu-icon-active').on('click', function () {
+  //   $(".wrap_test_unit").css("visibility", "visible");
+  // });
+
+
 
   $('.menu-icon-wrapper').on('click', function () {
     // $(".list_docs").css("opacity", "0.5");
@@ -17,6 +45,10 @@ $(document).ready(function () {
     $('.wrap_sidebar_menu-hidden').toggleClass('wrap_sidebar_menu-visible');
     $('.index_p').toggleClass('index_p-transparent');
     $('.display_off').toggleClass('.display_on');
+    // $(".wrap_test_unit-visible").toggleClass('.wrap_test_unit-hidden');
+
+    // $(".wrap_test_unit").css("visibility", "hidden");
+    HideTest();
 
 
 
